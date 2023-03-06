f8dde722c79ed1be08ff63d09f074846501b8b08
MUKUND THAKUR <92576255+Mukund05@users.noreply.github.com>
2023-03-06 23:43:59 +0530

    completed skipped question
    completed skipped question of next permutation of arrays part 1 q. no 3

--- Arrays Part 1/3_Next_Permutation.java
+++ Arrays Part 1/3_Next_Permutation.java
@@ -1,7 +1,34 @@
-// class Main{
-//     public static void main(String[] args) {
-        
-//     }
+ class Main{
+     public static void main(String[] args) {
+        public void solution(int[] nums) {
+        if(nums.length==1) return;
+        int idx=-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx!=-1){
+            int k=nums.length-1;
+            while(k>0 && nums[idx]>=nums[k]) k--;
+            if(k>0) swap(nums,idx,k);
+        }
+        reverse(nums, idx+1, nums.length-1);
+    }
 
-// }
-//skipped for sunday
+    private void reverse(int[] nums,int left,int right){
+        while(left<right){
+            swap(nums,left,right);
+            left++;
+            right--;
+        }
+    }
+    private void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+     }
+
+ }
